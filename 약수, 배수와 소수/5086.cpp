96075b1934dc2222e4f1e76936f424b77776221f
flatcalm <a_abnc@naver.com>
2023-12-07 11:45:24 +0900

    231206 - 5086 해결

---
+++ 약수, 배수와 소수/5086.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a = 1, b = 1;
+
+    while(a != 0 && b != 0) {
+        cin >> a >> b;
+        if (a < b) {
+            if (b % a == 0) {
+                cout << "factor" << "\n";
+            }
+            else {
+                cout << "neither" << "\n";
+            }
+        }
+        else if (a > b) {
+            if (a % b == 0) {
+                cout << "multiple" << "\n";
+            }
+            else {
+                cout << "neither" << "\n";
+            }
+        }
+    }
+}
