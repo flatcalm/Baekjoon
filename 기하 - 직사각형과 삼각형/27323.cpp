67062a4d557050ffa9f4f5dd535174b7ae9c383b
flatcalm <a_abnc@naver.com>
2023-12-14 10:04:54 +0900

    231214 - 27323 직사각형 해결

---
+++ 기하 - 직사각형과 삼각형/27323.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 직사각형
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a = 1, b = 1;
+
+    cin >> a >> b;
+
+    cout << a * b << "\n";
+}
